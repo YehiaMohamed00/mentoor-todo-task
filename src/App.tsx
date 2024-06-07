8c01f1f28665e4a7fa128b4ae9d5a4a9db4c0bce
Yehiaa <esc.yehia.mohamed1@gmail.com>
2024-06-07 14:46:24 +0300

    added todo list persistence using localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Task from './components/Task'
 
 function App() {
-  const [todos, setTodos] = useState([
-    {isFinished: false, text:"1st item"},
-    {isFinished: true, text:"2nd item"},
-  ]); 
+
+  interface Todo {
+    taskId: number;
+    isFinished:boolean
+    text: string;
+  }
+
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const storedTodos = localStorage.getItem("tasks");
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  });
+
   const [todo, setTodo] = useState("");
+
+  useEffect(()=>{
+    localStorage.setItem("tasks", JSON.stringify([...todos]));
+  },[todos]);
+
   return (
     <>
       <h1> To Do List 📃</h1>
       <form onSubmit={(event)=> {
           event.preventDefault();
-          setTodos([...todos, {isFinished: false, text: todo}])
+          setTodos([...todos, {taskId: todos.length + 1, isFinished: false, text: todo}])
           setTodo("");
         }}>
         <input type="text" style={{height:"30px", width:"60%"}} value={todo} onChange={(event)=>setTodo(event.target.value)}/> <button>add todo</button>
       </form>
-        {todos.map((task, index)=> <Task key={index} isFinished={task.isFinished} text={task.text}/>)}
+        <button onClick={()=> {localStorage.clear(); setTodos([])}}> clear all </button>
+        {todos.map((task)=> <Task key={task.taskId} isFinished={task.isFinished} text={task.text}/>)}
     </>
   )
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,22 @@
 
 interface Props{
-  key: number;
+  key:number;
   isFinished: boolean;
   text: string;
 }
 
-export default function Task({key, isFinished, text}:Props):JSX.Element{
+export default function Task({ key, isFinished, text}:Props):JSX.Element{
+
+  // TODO: Fix checkbox not registering in localStorage issue.
+
   return (<div >
-    <input type="checkbox" key={key} defaultChecked={isFinished}/>
+    <input type="checkbox" defaultChecked={isFinished} onChange={(event)=>{
+      const tasks:string | null = localStorage.getItem("tasks");
+      const tasksObj = JSON.parse(tasks!);
+      tasksObj[key].isFinished = event.target.checked;
+      localStorage.clear();
+      localStorage.setItem("tasks", JSON.stringify(tasksObj));
+    }}/>
     <p style={{display: "inline", fontSize:"1.5em"}}>{text}</p>
   </div>);
 }
